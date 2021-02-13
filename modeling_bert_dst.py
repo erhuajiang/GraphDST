9abff3de1e6c932c1081b0727d1b8e59c6178cac
fengyue <fengyue.fy@bytedance.com>
2021-02-13 14:48:48 +0000

    loss

--- modeling_bert_dst.py
+++ modeling_bert_dst.py
@@ -193,8 +193,6 @@ class BertForDST(BertPreTrainedModel):
                 if not self.refer_loss_for_nonpointable:
                     refer_loss *= token_is_referrable
                 
-                print(class_label_id[slot].cpu().data)
-                # print(class_logits.cpu().numpy())
                 class_loss = class_loss_fct(class_logits, class_label_id[slot])
 
                 if self.refer_index > -1:
