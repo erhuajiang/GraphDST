24b7fb80efb7310493d06dbe01e566a52e5d6940
fengyue <fengyue.fy@bytedance.com>
2021-02-25 11:57:45 +0000

    woz

--- DO.example.advanced.sh
+++ DO.example.advanced.sh
@@ -31,7 +31,7 @@ for step in train dev test; do
 	args_add="--do_eval --predict_type=${step}"
     fi
 
-    CUDA_VISIBLE_DEVICES=1 python3 run_dst.py \
+    CUDA_VISIBLE_DEVICES=0 python3 run_dst.py \
 	    --task_name=${TASK} \
 	    --data_dir=${DATA_DIR} \
 	    --dataset_config=dataset_config/${TASK}.json \
